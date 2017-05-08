04003ac35bfe8e5ffa66a3d944e9bee1dd05dd3e
hackerudroids <tomerjava@gmail.com>
2017-05-08 20:27:51 +0300

    Dynamic fragments Practice Finished

--- app/src/main/java/hackeru/edu/fragments/TheMainActivity.java
+++ app/src/main/java/hackeru/edu/fragments/TheMainActivity.java
@@ -3,18 +3,31 @@ package hackeru.edu.fragments;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 public class TheMainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
     BottomNavigationView navigationView;
+    FragmentManager manager;
+    ColorPickerFragment mColorPickerFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_the_main);
         navigationView = (BottomNavigationView) findViewById(R.id.navigation);
         navigationView.setOnNavigationItemSelectedListener(this);
+
+        manager = getSupportFragmentManager();
+
+        mColorPickerFragment = new ColorPickerFragment();
+
+        manager.beginTransaction().
+                add(R.id.frame1, mColorPickerFragment).
+                commit();
+
     }
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -22,13 +35,19 @@ public class TheMainActivity extends AppCompatActivity implements BottomNavigati
 
         switch (id){
             case R.id.navigation_color:
-
+               manager.beginTransaction().
+                       replace(R.id.frame1, mColorPickerFragment).
+                       commit();
                 return true;
             case R.id.navigation_text:
-
+                manager.beginTransaction().
+                        replace(R.id.frame1, new TextFragment()).
+                        commit();
                 return true;
             case R.id.navigation_notifications:
-
+                manager.beginTransaction().
+                        replace(R.id.frame1, new ColorPickerFragment()).
+                        commit();
                 return true;
         }
         return false;
