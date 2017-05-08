c9decbd458a994c849a75f19a6cea173d26715b8
hackerudroids <tomerjava@gmail.com>
2017-05-08 18:32:02 +0300

    finished basic Color Picker Fragment

--- app/src/main/java/hackeru/edu/fragments/MainActivity.java
+++ app/src/main/java/hackeru/edu/fragments/MainActivity.java
@@ -1,13 +1,39 @@
 package hackeru.edu.fragments;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    FragmentManager manager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //The Dynamic Approach:
+        //1) Get a reference to the fragment manager:
+
+        //add, remove, replace, find.
+        manager = getSupportFragmentManager();
+
+
+        manager.beginTransaction()
+                .replace(R.id.frame1, new ColorPickerFragment())
+                .commit();
+
+    }
+
+    public void text(View view) {
+        manager.beginTransaction()
+                .replace(R.id.frame1, new TextFragment())
+                .commit();
+    }
+
+    public void color(View view) {
+        manager.beginTransaction()
+                .replace(R.id.frame1, new ColorPickerFragment())
+                .commit();
     }
 }
